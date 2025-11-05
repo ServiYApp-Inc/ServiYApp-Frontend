cb48207d8c06354ab22d8ebcc9d2da5aa45cfa9c
AriadnaRamirez <ariadnamts98@gmail.com>
2025-11-04 22:53:57 -0600

    Fix en importacion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,36 +1,35 @@
+import IUser from "../interfaces/IUser";
 import { Api } from "./api";
-import { Role, User } from "@/app/store/auth.store";
+import { Role } from "@/app/store/auth.store";
 
 type LoginDto = {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 };
 
 export type AuthPayload = {
-  token: string;
-  role: Role;
-  user: User;
+	token: string;
+	role: Role;
+	user: IUser;
 };
 
 export const AuthService = {
-  async login(role: Role, dto: LoginDto): Promise<AuthPayload> {
-    const endpoint =
-      role === "provider"
-        ? "/auth/login/provider"
-        : "/auth/login/user";
+	async login(role: Role, dto: LoginDto): Promise<AuthPayload> {
+		const endpoint =
+			role === "provider" ? "/auth/login/provider" : "/auth/login/user";
 
-    const { data } = await Api.post(endpoint, dto);
+		const { data } = await Api.post(endpoint, dto);
 
-    // 🔸 Ajustamos según la estructura real de tu backend
-    return {
-      token: data.access_token,
-      role: role,
-      user: data.provider || data.user,
-    };
-  },
+		// 🔸 Ajustamos según la estructura real de tu backend
+		return {
+			token: data.access_token,
+			role: role,
+			user: data.provider || data.user,
+		};
+	},
 
-  startGoogle(role: Role) {
-    const base = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-    window.location.href = `${base}/auth/google/start?role=${role}`;
-  },
+	startGoogle(role: Role) {
+		const base = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+		window.location.href = `${base}/auth/google/start?role=${role}`;
+	},
 };
