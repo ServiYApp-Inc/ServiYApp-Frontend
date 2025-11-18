5d9a839aa1eb015ea0ca21a687051e977d4b85f0
Stefano Masotti <stefanovmasotti@gmail.com>
2025-11-18 00:06:17 -0300

    Fixes

--- src/app/(app)/admin/dashboard/categories/page.tsx
+++ src/app/(app)/admin/dashboard/categories/page.tsx
@@ -7,6 +7,7 @@ import { ICategory } from "@/app/interfaces/ICategory";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/navigation";
+import Swal from "sweetalert2";
 
 
 export default function CategoriesPage() {
@@ -38,6 +39,41 @@ export default function CategoriesPage() {
             </div>
         );
 
+    const handleUpdate = () => {
+
+    }
+
+    const handleDelete = async () => {
+        try {
+            const result = await Swal.fire({
+            title: "¿Estás seguro?",
+            text: "Si aceptas, se eliminará la categoría para siempre",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#1d2846",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Sí, eliminar",
+            });
+
+            if (result.isConfirmed) {
+                // Endpoint Delete Category
+            await Swal.fire({
+                title: "Categoría eliminada",
+                text: res.message || "La categoría fue eliminado con éxito.",
+                icon: "success",
+            });
+
+            }
+        } catch (error) {
+            console.error("Error al eliminar categoría:", error);
+            Swal.fire({
+            title: "Error",
+            text: "No se pudo eliminar la categoría. Verifica tu rol o inténtalo nuevamente.",
+            icon: "error",
+            });
+        }
+        };
+
     return (
         <main className="flex flex-col justify-start bg--background overflow-x-hidden overflow-y-hidden min-h-screen px-2 pb-20 md:pb-4 max-w-[1300px] mx-auto">
             <h1 className="font-bold text-[var(--color-primary)] text-[48px] mt-10 text-center md:text-left">
@@ -61,9 +97,13 @@ export default function CategoriesPage() {
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-4 mt-4">
                 {category.map((c) => (
-                    <div key={c.id} className="flex flex-col text-center bg-white p-2 rounded-xl">
-                        <span>{c.name}</span>
-                        <span>{c.description}</span>
+                    <div key={c.id} className="flex flex-col text-center bg-white p-2 rounded-xl shadow">
+                        <span className="text-[var(--color-primary)] font-bold text-lg border-b border-[var(--color-primary)]">{c.name}</span>
+                        <span className="text-gray-500 ">{c.description}</span>
+                        <div className="flex flex-row justify-around my-2">
+                        <button onClick={handleUpdate} className="text-white py-1 px-2 bg-[var(--color-primary)] hover:bg-[var(--color-primary-hover)] rounded-lg">Modificar</button>
+                        <button onClick={handleDelete} className="text-white py-1 px-2 bg-red-800 hover:bg-red-700 rounded-lg">Eliminar</button>
+                        </div>
                     </div>
                 ))}
             </div>
